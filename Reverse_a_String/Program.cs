fe685b931683fa4969c781e56e40a17a68d7af30
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-12-29 23:01:24 -0800

    Reverse a String
    I have created an application that gets a string from the user and then
    simply reverses it. The string will then be displayed back to the user.

---
+++ Reverse_a_String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.Write("Please enter a string to reverse: ");
+		var originalString = Console.ReadLine();
+
+		if (String.IsNullOrEmpty(originalString))
+		{
+			Console.WriteLine("You did not enter a string.");
+		}
+		else
+		{
+			//reverse the string
+
+		}
+	}
+}
