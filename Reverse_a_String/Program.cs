37977167594d9c96d66e2911a5138b9478374048
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-12-30 00:09:07 -0800

    Need to refactor the code

--- Reverse_a_String/Program.cs
+++ Reverse_a_String/Program.cs
@@ -2,19 +2,50 @@
 
 class Program
 {
+
+	public void reverseTheString()
+	{
+		//reverse the string
+		char[] originalStringArray = originalString.ToCharArray();
+		Array.Reverse(originalStringArray);
+		string reversedString = new string(originalStringArray);
+		Console.WriteLine($"The string reversed is: {reversedString}");
+	}
+
+
 	static void Main(string[] args)
 	{
 		Console.Write("Please enter a string to reverse: ");
 		var originalString = Console.ReadLine();
 
+
 		if (String.IsNullOrEmpty(originalString))
 		{
 			Console.WriteLine("You did not enter a string.");
+
+			Console.Write("Please enter a string to reverse again: ");
+			originalString = Console.ReadLine();
+
+
+			//reverse the string
+			char[] originalStringArray = originalString.ToCharArray();
+			Array.Reverse(originalStringArray);
+			string reversedString = new string(originalStringArray);
+			Console.WriteLine($"The string reversed is: {reversedString}");
+
+			Console.ReadLine();
 		}
 		else
 		{
 			//reverse the string
+			char[] originalStringArray = originalString.ToCharArray();
+			Array.Reverse(originalStringArray);
+			string reversedString = new string(originalStringArray);
+			Console.WriteLine($"The string reversed is: {reversedString}");
 
+			Console.ReadLine();
 		}
+
 	}
+
 }
